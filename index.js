e391c2ad296e92eb0da60bd10befd1a55f5d39fb
eugenio <1evsantos8@gmail.com>
2018-11-07 17:14:33 +0000

    jekly working and gitignore

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ function run() {
     xhr.open('GET', url);
     xhr.onreadystatechange = function () {
       if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
-    };
+    }
+      ;
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
     return xhr;
   }
-
-
-
   window.onhashchange = function (event) {
     console.log(location.hash, event);
     if (location.hash === '') {
+      document.getElementById('nav-div').style.display = 'none';
       document.getElementById('center-col').style.display = 'block';
       document.getElementById('right-col').style.display = 'block';
     }
     if (location.hash === '#photo') {
       document.getElementById('center-col').style.display = 'none';
       document.getElementById('right-col').style.display = 'none';
-      getAjax('./t.html', function (data) {
-        console.log(data);
-
+      getAjax('./photo/photo.html', function (data) {
+        document.getElementById('nav-div').innerHTML = data;
+        document.getElementById('nav-div').style.display = 'block';
       })
     }
-  };
+  }
+    ;
 }
 
 if (document.readyState != 'loading') run();
@@ -35,4 +35,6 @@ else if (document.addEventListener) document.addEventListener('DOMContentLoaded'
 // IE <= 8
 else document.attachEvent('onreadystatechange', function () {
   if (document.readyState == 'complete') run();
-});
+}
+
+);
